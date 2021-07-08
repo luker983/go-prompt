c7b0367ab08b75a1497bba530a37567a5b48eed4
luker983 <lukerindels983@gmail.com>
2021-07-08 09:38:12 -0600

    Rename c-bata -> luker983

--- _example/http-prompt/main.go
+++ _example/http-prompt/main.go
@@ -10,7 +10,7 @@ import (
 	"path"
 	"strings"
 
-	prompt "github.com/c-bata/go-prompt"
+	prompt "github.com/luker983/go-prompt"
 )
 
 type RequestContext struct {
--- internal/bisect/bisect_test.go
+++ internal/bisect/bisect_test.go
@@ -5,7 +5,7 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/c-bata/go-prompt/internal/bisect"
+	"github.com/luker983/go-prompt/internal/bisect"
 )
 
 func Example() {
